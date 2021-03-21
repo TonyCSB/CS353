bbcc6f0cae0f4cf27d3c832c3a74a842715dd8fb
Tony Chen <git@tonychen.page>
2021-03-21 21:45:25 +0800

    Module 2 complete

--- project1/718030290013_陈思贝_project1_src/homework1.c
+++ project1/718030290013_陈思贝_project1_src/homework1.c
@@ -1,11 +1,28 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/kernel.h>
 
 MODULE_LICENSE("GPL");
 
+static int paramInt;
+static char *paramStr;
+static int paramArr[3];
+static int pr_arr;
+
+module_param(paramInt, int, 0644);
+module_param(paramStr, charp, 0);
+module_param_array(paramArr, int, &pr_arr, 0444);
+
 static int __init hello_init(void) {
+    int i;
+
     printk(KERN_INFO "Hello, world\n");
+    printk(KERN_INFO "Param-int:%d;\n", paramInt);
+    printk(KERN_INFO "Param-str:%s;\n", paramStr);
+    for (i = 0; i < (sizeof paramArr / sizeof (int)); i++) {
+		printk(KERN_INFO "Param-arr[%d]: %d\n", i, paramArr[i]);
+	}
     return 0;
 }
 
--- project1/718030290013_陈思贝_project1_src/test.sh
+++ project1/718030290013_陈思贝_project1_src/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sudo bash
 
 make
-insmod homework1.ko
+insmod homework1.ko paramInt=1231312 paramStr='"test string"' paramArr='1,2,3'
 rmmod homework1.ko
 tail /var/log/kern.log
 make clean
